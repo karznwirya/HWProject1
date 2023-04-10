5ee8e1db281451aed31911f6670deaa366f89d3b
karza <karza@10.0.0.52>
2023-04-10 11:20:14 -0400

    IMlementgetinfo()

--- src/Demo1/Test1.java
+++ src/Demo1/Test1.java
@@ -5,5 +5,9 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Test1");
 	}
+	
+	public void getinfo1() {
+		System.out.println("Pushing scond time");
+	}
 
 }
